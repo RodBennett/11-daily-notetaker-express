9d8fc62cc01cd7aa826ca581854311f4270adf07
Rod Bennett <rodbennett@Rods-Air-2.lan>
2022-09-07 19:23:55 -0600

    post and get methods working

--- server.js
+++ server.js
@@ -1,65 +1,72 @@
 // setting up server through npm express.js
 const express = require('express');
 const path = require('path');
-const storedNotes = require('./db/db.json')
-const PORT = process.env.PORT || 3001;
-const fs = require('fs')
+const fs = require('fs');
 
+// importing helper code to generate random id numbers for notes
+const uuid = require('./helpers/uuid.js')
+
+
+// getting express js middleware for post/get/delete methods
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// import express.js
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+const PORT = process.env.PORT || 3001;
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 // GET route for notes.html linked to homepage
-app.get('/notes', (req, res) => 
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET route for db.json notes file
-app.get('/api/notes', (req, res) => res.json(storedNotes),
-)
+app.get('/api/notes', (req, res) => { 
+  res.sendFile(path.join(__dirname, '/db/db.json'))
+});
 
 //GET route for specifically searched note
-app.get('/api/notes/:title', (req, res) => {
-  const note = req.params.title;
-
-//loop to find requested title among all titles in db.json
-  for (let i = 0; i < note.length; i++) {
-    if(requestedNote === note[i].title) {
-      console.log(note, "Hello")
-      res.json(fs.writeFile(`./db${note}`))
-    }
-  }
-  return res.json("No note found.")
-})
+app.get('/api/notes/:id', (req, res) => {
+  const selectedNote = req.params.title;
+  res.json(selectedNote)
+});
 
 // POST route for creating notes
-app.post('/api/notes', (req,res) => {
-  const { title, text } = req.body;
-  if(title && text) {
-    const newNote = {
-      title,
-      text,
-    };
+app.post('/api/notes', (req, res) => {
 
-  const response = {
-    status: "success",
-    body: newNote,
-  };
-  const myJSON = JSON.stringify(response)
-  console.log(myJSON);
-  res.json(fs.writeFile('./db/db.json', myJSON, (err) => {
+  // variable to add note using req.body, which is a property that uses body of objects in db.json
+  const addNote = req.body;
+  // this adds the id property to new notes, which calls the uuid function in the helper code to generate random id numbers for notes
+  addNote.id = uuid();
+  // the readFileSync method is used for the data in the note to 
+  let noteData = fs.readFileSync('./db/db.json')
+  // parses the data in the new note 
+  let noteReceiver = JSON.parse(noteData)
+  // pushes data from new note in its body form (key, value) to a variable
+  noteReceiver.push(req.body);
+  // writes the data to db.json, data is stringified for JSON format, and "null, 2" is used to format it with indentations in db.json
+  fs.writeFileSync('./db/db.json', JSON.stringify(noteReceiver, null, 2), err => {
     if (err) throw err;
-    console.log('The file has been saved!');
-  })
-)}
-})
+    res.json(noteReceiver)
+   });
+   // response function to send data from db.json to html page
+   res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
 
+// GET notes on HTML page at client's request
+app.get('/notes', (req, res) => res.sendFile(__dirname, '/public/notes/html'))
+app.get('*', (req, res) => res.sendFile(__dirname, '/public/index.html'))
+
+// DELETE notes
+// app.delete('./api/notes/:id', (req, res) => {
+//   let noteData = fs.readFileSync('./db/db.json');
+//   let noteReceiver = JSON.parse(noteData);
+
+// })
+
+// set up port for listening and creates link for execution in terminal
 app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`));
